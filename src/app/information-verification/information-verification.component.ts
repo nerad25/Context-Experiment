126aba9ab7bd11b113b4c1b8976ae7adbb0f6590
nerad25 <dmcrull@yahoo.com>
2020-09-29 15:36:30 -0500

    added information-verification component and submit data component

---
+++ src/app/information-verification/information-verification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-information-verification',
+  templateUrl: './information-verification.component.html',
+  styleUrls: ['./information-verification.component.scss']
+})
+export class InformationVerificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
